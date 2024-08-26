2ef61946500367b1dfedd00ef6a90a503a02548f
Dreyc20 <157500344+Dreyc20@users.noreply.github.com>
2024-08-26 09:35:43 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,172 @@
+export const perguntas = [
+    {
+        enunciado: "Com a crescente inovação na tecnologia de veículos elétricos e o aumento da rede de estações de carregamento, a adoção desses veículos está se tornando mais viável. Será que essa tendência continuará a crescer, tornando os carros elétricos a principal escolha para os consumidores?",
+        alternativas: [
+            {
+                texto: "Sim, a tendência de adoção dos carros elétricos provavelmente continuará a crescer.",
+                afirmacao: [
+                    "Viu beneficios no aumento constante de carros elétricos e das estações de carregamento.",
+                    "Afirmou que os carros elétricos irá crescer."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Não necessariamente.",
+                afirmacao: [
+                    "Percebeu algum problema em relação ao crescimento das estações de carregamento.",
+                    "Pensou em algumas desvantagens."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Com o crescente número de fabricantes investindo em carros autônomos e as melhorias contínuas na tecnologia de sensores e inteligência artificial, os veículos autônomos estão se aproximando da realidade comercial. Será que a introdução em larga escala desses carros autônomos pode transformar a maneira como nos deslocamos no dia a dia?",
+        alternativas: [
+            {
+                texto: "Sim, a introdução em larga escala de carros autônomos pode transformar significativamente a maneira como nos deslocamos.",
+                afirmacao: [
+                    "Conseguiu ver as vantagens da automação dos carros.",
+                    "Percebeu que a implantação dessas tecnologias irá facilitar o deslocamento diário.",
+                    "Percebeu a capacidade desses veículos de operar sem intervenção humana, podemos esperar uma redução nos acidentes de trânsito, melhorias na eficiência do tráfego e mais tempo livre para os passageiros."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não necessariamente.",
+                afirmacao: [
+                    "Sentiu uma insegurança ao se tratar de uma atutomação.",
+                    "Achou que introdução em larga escala de carros autônomos pode enfrentar vários desafios antes de se tornar uma realidade transformadora.",
+                    "Sentiu a infraestrutura atual pode não estar completamente preparada para suportar a operação de veículos autônomos, o que pode limitar sua capacidade de transformar o transporte de maneira ampla e imediata."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Com o crescimento das preocupações ambientais e a implementação de regulamentações mais rigorosas, muitos países estão incentivando o uso de veículos elétricos. Será que esses incentivos serão suficientes para substituir os carros movidos a combustíveis fósseis em um futuro próximo?",
+        alternativas: [
+            {
+                texto: "Sim, pois isso inovará o mercado.",
+                afirmacao: [
+                    "Conseguiu perceber que á incetivos governamentais na regulamentação ambiental promovendo adoção rápida de carros elétricos.",
+                    "Percebeu que pode acelerar a substituição dos carros movidos a combustíveis fósseis.",
+                    "Percebeu isenções fiscais e investimentos em infraestrutura de carregamento, muitos consumidores estão sendo atraídos para os carros elétricos, e a contínua redução dos custos de produção e melhorias na tecnologia devem tornar essa transição mais viável no futuro próximo."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não necessariamente, pois não concordo com certos pontos.",
+                afirmacao: [
+                    "Sentiu que apesar dos incentivos, a substituição completa dos carros movidos a combustíveis fósseis pode enfrentar obstáculos significativos.",
+                    "Achou que os altos custos iniciais dos veículos elétricos, a infraestrutura de carregamento ainda limitada em algumas áreas e a resistência de consumidores habituados aos combustíveis fósseis podem retardar a adoção em larga escala.",
+                    "Sentiu  os desafios relacionados à produção de baterias e à reciclagem também podem influenciar a velocidade dessa transição."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: " À medida que as montadoras lançam novos modelos de carros elétricos com maior autonomia e melhores características, a demanda por esses veículos está crescendo. Será que esse aumento na demanda vai impulsionar a indústria a investir ainda mais em tecnologias de carregamento rápido e infraestrutura de recarga?",
+        alternativas: [
+            {
+                texto: " Sim, pois com isso terá um avanço e o número de aquisições irá aumentar.",
+                afirmacao: [
+                    "O aumento na demanda por carros elétricos provavelmente incentivará a indústria a investir mais em tecnologias de carregamento rápido e infraestrutura de recarga. Com consumidores exigindo mais conveniência e eficiência, as montadoras e empresas de energia estarão motivadas a desenvolver soluções que atendam a essas necessidades.",
+                    "Tudo isso pode resultar em uma rede de recarga mais robusta e em avanços tecnológicos que tornam os veículos elétricos ainda mais atraentes para o público em geral."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não, pois isso levará um aumento nos custos e poderá reduzir o número de compras.",
+                afirmacao: [
+                    "Sua preocupação é que sem um apoio coordenado e substancial, a infraestrutura de recarga pode não evoluir na mesma velocidade que o crescimento da demanda.",
+                    "Problemas como a alocação de recursos, a complexidade na implementação de redes de recarga e o custo elevado de tecnologias avançadas podem limitar o progresso."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Com a crescente popularidade dos carros elétricos e as promessas de uma redução significativa nas emissões de carbono, muitos acreditam que eles são a solução para problemas ambientais. Será que a adoção massiva desses veículos pode realmente ter um impacto profundo na redução das emissões globais de gases de efeito estufa?",
+        alternativas: [
+            {
+                texto: "Sim, a adoção massiva de carros elétricos pode ter um impacto significativo na redução das emissões globais de gases de efeito estufa.",
+                afirmacao: [
+                    "Os veículos elétricos não emitem poluentes diretamente e, se forem carregados com energia proveniente de fontes renováveis, seu impacto ambiental é consideravelmente menor em comparação aos veículos movidos a combustíveis fósseis.",
+                    "Essa mudança em larga escala pode contribuir substancialmente para a redução das emissões e ajudar a combater as mudanças climáticas."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não, embora os carros elétricos reduzam as emissões diretamente associadas ao transporte, o impacto global na redução das emissões de gases de efeito estufa pode ser limitado.",
+                afirmacao: [
+                    "Sua preocupação ocorre porque a produção de baterias para veículos elétricos e a geração de eletricidade para carregamento podem também gerar emissões, especialmente se a energia elétrica provém de fontes não renováveis.",
+                    "A transição para veículos elétricos pode não ser rápida o suficiente para compensar o aumento geral das emissões e o crescimento da frota global de veículos."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Com a inovação constante em tecnologia de baterias e a introdução de novas formas de mobilidade, como os veículos elétricos e autônomos, muitos se perguntam sobre o futuro do transporte urbano. Será que essas inovações podem resolver os problemas de congestionamento e poluição nas grandes cidades?",
+        alternativas: [
+            {
+                texto: "Sim, essas inovações têm o potencial de resolver problemas de congestionamento e poluição nas grandes cidades.",
+                afirmacao: [
+                    "Veículos elétricos podem reduzir as emissões de poluentes, enquanto tecnologias autônomas podem otimizar o fluxo de tráfego e diminuir congestionamentos ao permitir uma coordenação mais eficiente entre os veículos.",
+                    "O avanço na tecnologia de baterias pode tornar os veículos elétricos mais acessíveis e práticos, incentivando uma maior adoção e contribuindo para um ambiente urbano mais limpo e menos congestionado."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: " Não, apesar das inovações em tecnologia de baterias e veículos autônomos, a resolução dos problemas de congestionamento e poluição pode ser limitada.",
+                afirmacao: [
+                    "A integração dessas tecnologias pode enfrentar desafios como altos custos, resistência à mudança e a necessidade de uma infraestrutura urbana completamente adaptada.",
+                    "O aumento da população urbana e o crescimento contínuo da frota de veículos podem contrabalançar os benefícios, dificultando a eliminação completa dos problemas de congestionamento e poluição."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Com o avanço dos veículos autônomos e o crescente foco em tornar a mobilidade mais acessível para todas as idades, há uma expectativa de que essas tecnologias possam beneficiar especialmente os idosos. Será que a popularização dos carros autônomos pode realmente transformar a mobilidade dos idosos e melhorar sua qualidade de vida?",
+        alternativas: [
+            {
+                texto: "Sim, a popularização dos carros autônomos pode transformar significativamente a mobilidade dos idosos e melhorar sua qualidade de vida.",
+                afirmacao: [
+                    "Esses veículos podem oferecer uma solução prática e segura para aqueles que não podem mais dirigir devido a limitações físicas ou cognitivas.",
+                    "Com a condução autônoma, os idosos poderão manter a independência e a mobilidade sem depender de terceiros, facilitando o acesso a atividades sociais e serviços essenciais."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Não, embora os carros autônomos ofereçam a promessa de maior independência para os idosos, a realidade pode ser mais complexa.",
+                afirmacao: [
+                    "Sua preocupação com a tecnologia de veículos autônomos ainda está em desenvolvimento e pode não estar amplamente disponível ou acessível para todos os idosos no curto prazo.",
+                    "A adaptação a novas tecnologias pode ser desafiadora para alguns, e questões de custo e infraestrutura podem limitar o impacto positivo desses veículos na vida dos idosos."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Com a crescente adoção de políticas de planejamento urbano sustentável e a criação de espaços verdes nas cidades, muitos acreditam que essas mudanças podem melhorar significativamente a qualidade de vida urbana. No entanto, será que essas medidas são suficientes para resolver todos os problemas de desigualdade e bem-estar nas áreas urbanas, ou estamos apenas fazendo ajustes superficiais?",
+        alternativas: [
+            {
+                texto: "As políticas de planejamento urbano sustentável e a criação de espaços verdes podem, de fato, melhorar significativamente a qualidade de vida nas cidades.",
+                afirmacao: [
+                    "Notou que esses esforços ajudam a criar ambientes mais saudáveis e acessíveis, promovem o bem-estar mental e físico dos residentes e incentivam um estilo de vida mais equilibrado.",
+                    "Ainda acha que ao integrar espaços verdes e práticas sustentáveis no planejamento urbano, as cidades podem se tornar mais inclusivas e resilientes, abordando questões como poluição e desigualdade de maneira mais eficaz."
+                ],
+            },
+            {
+                texto: "Não, o planejamento urbano sustentável e a criação de espaços verdes sejam passos positivos, essas medidas podem ser insuficientes para resolver problemas mais profundos de desigualdade e bem-estar nas áreas urbanas.",
+                afirmacao: [
+                    "Acelerou o processo de desigualdade social e econômica muitas vezes exige reformas mais abrangentes, como melhorias no acesso à educação e saúde, políticas habitacionais inclusivas e redistribuição de recursos.",
+                    "Percebeu que sem abordar essas questões fundamentais, os ajustes superficiais podem não ter um impacto duradouro ou significativo na qualidade de vida urbana para todos os residentes.."
+                ],
+            },
+        ]
+    }
+];
